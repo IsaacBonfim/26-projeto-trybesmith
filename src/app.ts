bf5a6ac0048f37efb722bbc4444b17948651b165
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-08-03 21:27:16 -0300

    Enviando requisito 3

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import pRoutes from './routes/products.routes';
+import uRoutes from './routes/users.routes';
 import errors from './middleware/error.middleware';
 
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/products', pRoutes);
+app.use('/users', uRoutes);
 
 app.use(errors);
 
