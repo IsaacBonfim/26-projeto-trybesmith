ff17305ac91716aff2d1e2580ef693d854e72015
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-08-03 21:17:59 -0300

    feat: inserido no arquivo users.controller.ts a função create

---
+++ src/controllers/users.controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import JWT from 'jsonwebtoken';
+import Service from '../services/users.service';
+
+class UserController {
+  constructor(private uService = new Service()) { }
+
+  public create = async (req: Request, res: Response) => {
+    const { username } = await this.uService.create(req.body);
+    const secret = 'TheGreatMrOliv3ira';
+    const token = JWT.sign({ data: username }, secret);
+
+    res.status(201).json({ token });
+  };
+}
+
+export default UserController;
